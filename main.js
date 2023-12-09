057294b8137696a9378c82a1344e60c478022455
vitodursopettersson <vitodurso98@gmail.com>
2023-12-09 17:40:31 +0100

    feat: random email generator

--- main.js
+++ main.js
@@ -5,7 +5,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            randomEmail: []
+        }
+    },
+    created() {
+
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((responses) => {
+
+                this.randomEmail.push(responses.data.response)
+
+            })
 
         }
     },
+    mounted() {
+        console.log(this.randomEmail)
+    }
 }).mount('#app')
